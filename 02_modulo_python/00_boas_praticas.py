a22ccdf812ebb873f4b914ec3f146660238885dd
ramiromanoel <ramiro.junior1@gmail.com>
2024-04-11 16:36:34 -0300

    boas praticas

---
+++ 02_modulo_python/00_boas_praticas.py
@@ -0,0 +1,20 @@
+"""
+- O padrão de nomes deve ser snake case.
+- Escolher nomes sugestivos. Importante nomear de acordo com a função do código. 
+- Nome de constantes todos em maiúsculo.
+"""
+nome = "Ramiro"
+idade = 27
+
+print(nome, idade)
+
+nome, idade = "Novo Nome", 29
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+limite_saque_diario = 10
+
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"]
+
+print(BRAZILIAN_STATES)
